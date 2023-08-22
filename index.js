d09633482b51574fbfd1d92ee056b95b49faf6e8
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-08-22 15:19:30 -0700

    created inquirer and write file functions

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+function createLogo(data) {
+
+}
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Please enter the text for your logo. Please only use at most 3 characters.",
+            name: 'logoTxt',
+        },
+        {
+            type: 'input',
+            message: "Please enter your desired text color. This can be a color's name or a hexadecimal.",
+            name: 'logoTxtColor',
+        },
+        {
+            type: 'list',
+            message: "Which shape would you like your logo to be?",
+            name: 'logoShape',
+            choices: ['Circle', 'Triangle', 'Square'],
+        },
+        {
+            type: 'input',
+            message: "Please enter your desired logoShape Color. This can be a color's name or a hexadecimal.",
+            name: 'logoShapeColor',
+        },
+    ]).then(data => {
+        let logoGenerated = createLogo(data);
+
+        // writes the file to the dist folder as a .md, using the readMeGenerated as its content.
+        fs.writeFile('./examples/logo.svg', logoGenerated, (err) =>
+            err ? console.log(err) : console.log('Generated logo.svg')
+        )
+    });
