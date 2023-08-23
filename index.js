042de8b50ef902a25d1ad814d6cd12e3b6551a11
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-08-22 21:30:19 -0700

    added shapes.js classes and render function

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-function createLogo(data) {
+const Circle = require('./lib/shapes.js');
+const Triangle = require('./lib/shapes.js');
+const Square = require('./lib/shapes.js');
 
-}
+// function createLogo(data) {
+//     return `
+//     <rect x="10" y="10" width="30" height="30" stroke="${data.logoShapeColor}" stroke-width="5"/>
+//     `
+// }
 
 inquirer
     .prompt([
@@ -29,10 +35,20 @@ inquirer
             name: 'logoShapeColor',
         },
     ]).then(data => {
-        let logoGenerated = createLogo(data);
+        if(data.logoShape == 'Circle') {
+            logoGenerated = new Circle(data.logoShape, data.logoShapeColor);
+            finalLogo = logoGenerated.render(); 
+        } else if (data.logoShape == 'Triangle') {
+            logoGenerated = new Triangle(data.logoShape, data.logoShapeColor);
+            finalLogo = logoGenerated.render();
+        } else if (data.logoShape == 'Square') {
+            logoGenerated = new Square(data.logoShape, data.logoShapeColor);
+            finalLogo = logoGenerated.render();
+        }
+    
 
         // writes the file to the dist folder as a .md, using the readMeGenerated as its content.
-        fs.writeFile('./examples/logo.svg', logoGenerated, (err) =>
+        fs.writeFile('./examples/logo.svg', finalLogo, (err) =>
             err ? console.log(err) : console.log('Generated logo.svg')
         )
     });
