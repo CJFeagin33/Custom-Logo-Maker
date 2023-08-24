e65da368f8fb0bdb531ddadf7fb72a7b6dfe2adc
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-08-23 18:34:10 -0700

    added correct svg templates for render function

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const Circle = require('./lib/shapes.js');
-const Triangle = require('./lib/shapes.js');
-const Square = require('./lib/shapes.js');
-
-// function createLogo(data) {
-//     return `
-//     <rect x="10" y="10" width="30" height="30" stroke="${data.logoShapeColor}" stroke-width="5"/>
-//     `
-// }
+const Shape = require('./lib/Shape.js')
+const Circle = require('./lib/Circle.js');
+const Triangle = require('./lib/Triangle.js');
+const Square = require('./lib/Square.js');
 
 inquirer
     .prompt([
@@ -35,20 +30,16 @@ inquirer
             name: 'logoShapeColor',
         },
     ]).then(data => {
-        if(data.logoShape == 'Circle') {
-            logoGenerated = new Circle(data.logoShape, data.logoShapeColor);
-            finalLogo = logoGenerated.render(); 
-        } else if (data.logoShape == 'Triangle') {
-            logoGenerated = new Triangle(data.logoShape, data.logoShapeColor);
-            finalLogo = logoGenerated.render();
-        } else if (data.logoShape == 'Square') {
-            logoGenerated = new Square(data.logoShape, data.logoShapeColor);
-            finalLogo = logoGenerated.render();
+        if (data.logoShape === 'Circle') {
+            logoGenerated = new Circle(data.logoTxt, data.logoTxtColor, data.logoShapeColor).render();
+        } else if (data.logoShape === 'Triangle') {
+            logoGenerated = new Triangle(data.logoTxt, data.logoTxtColor, data.logoShapeColor).render();
+        } else if (data.logoShape === 'Square') {
+            logoGenerated = new Square(data.logoTxt, data.logoTxtColor, data.logoShapeColor).render();
         }
-    
 
         // writes the file to the dist folder as a .md, using the readMeGenerated as its content.
-        fs.writeFile('./examples/logo.svg', finalLogo, (err) =>
+        fs.writeFile('./examples/logo.svg', logoGenerated, (err) =>
             err ? console.log(err) : console.log('Generated logo.svg')
         )
     });
